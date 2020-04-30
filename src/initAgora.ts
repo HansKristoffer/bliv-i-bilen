718c9bafb5056f9b6af0b14f665dfbc235ff4128
Hans Kristoffer <hans@outscale.dk>
2020-04-30 16:38:01 +0200

    u

---
+++ src/initAgora.ts
@@ -0,0 +1,27 @@
+import * as AgoraRTC from 'agora-rtc-sdk'
+
+export function initAgora(role: "audience" | "host") {
+  const appId = 'a62035647b4f46b4acc82a95879f3ed8'
+  const channel = 'bliv-i-bilen'
+
+  const client = AgoraRTC.createClient({mode: "live", codec: "h264"})
+  client.init(appId, () => {
+    console.log("init success")
+
+    // subscribeStreamEvents()
+
+    client.setClientRole(role)
+
+    client.join(null, channel, null, (uid) => {
+      console.log("join channel: " + channel + " success, uid: " + uid)
+      this.rtc.params.uid = uid
+
+    }, function(err) {
+      console.error("client join failed", err)
+    })
+  }, (err) => {
+    console.error(err)
+  })
+
+  return client
+}
---
+++ src/listen.ts
@@ -0,0 +1,46 @@
+import * as AgoraRTC from 'agora-rtc-sdk'
+import { initAgora } from './initAgora'
+
+interface OutscaleLive {
+  rtcClient: AgoraRTC.Client
+  status: 'Playing' | 'Init' | 'Paused'
+}
+
+function outscaleLive(): OutscaleLive {
+  const client = initAgora('audience')
+
+  client.on("stream-added", (evt) => {
+    let stream = evt.stream
+    console.log("New stream added: " + stream.getId())
+    console.log("At " + new Date().toLocaleTimeString())
+    console.log("Subscribe ", stream)
+
+    client.subscribe(stream)
+  })
+  client.on("peer-leave", function(evt) {
+    console.log("Peer has left: " + evt.uid)
+    console.log(new Date().toLocaleTimeString())
+    console.log(evt)
+    // rt.removeStream(evt.uid)
+  })
+  client.on("stream-subscribed", function(evt) {
+    let stream = evt.stream
+    console.log("Got stream-subscribed event")
+    console.log(new Date().toLocaleTimeString())
+    console.log("Subscribe remote stream successfully: " + stream.getId())
+    console.log(evt)
+    // rt.addStream(stream)
+  })
+  client.on("stream-removed", function(evt) {
+    let stream = evt.stream
+    console.log("Stream removed: " + stream.getId())
+    console.log(new Date().toLocaleTimeString())
+    console.log(evt)
+    // rt.removeStream(stream.getId())
+  })
+
+  return {
+    status: 'Init',
+    rtcClient: client
+  }
+}
